6a5d5e910c758e063255c5c572821b0cb909e435
rodrimen <rodrimen@amdocs.com>
2023-10-16 11:58:34 -0600

    Initial commit

---
+++ src/test/java/com/emailmodule/EmailmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
