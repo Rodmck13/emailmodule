1d867ba4d3285f5e3aec0ab65f52ee36b4ee150d
rodrimen <rodrimen@amdocs.com>
2023-10-16 12:53:27 -0600

    Second commit

--- src/main/java/com/emailmodule/EmailmoduleApplication.java
+++ src/main/java/com/emailmodule/EmailmoduleApplication.java
@@ -1,13 +1,25 @@
 package com.emailmodule;
 
+import com.emailmodule.service.emailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class EmailmoduleApplication {
 
+	@Autowired
+	private emailSenderService senderService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmailmoduleApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendEmail(){
+		senderService.sendEmail("emailsendermodule@gmail.com", "Primera prueba", "Hola");
 	}
 
 }
